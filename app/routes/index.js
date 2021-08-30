d59e57e43d236d155821b8e0dc4a6a46028c7f64
kevin fernandez <kevinfernandezsoftdev@gmail.com>
2021-08-29 22:19:29 -0400

    adding index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,7 @@ const touristRouters = require('./tourists');
 const guidersRouters = require('./guiders');
 const locationRouters = require('./locations');
 
-
+router.get('/', (req,res) => { res.send("api is working")})
 router.use('/api/guiders', guidersRouters);
 
 router.use('/api/tourists', touristRouters);
